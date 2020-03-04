8222a17ddd0ed3b1e9a372142197b9be5d4e6b47
Alejandro Colomar <colomar.6.4.3@gmail.com>
2020-03-04 08:37:51 +0100

    Add scripts or loading environment variables

---
+++ cam/env.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/sh
+
+export	ROB_ADDR=rob
+export	ROB_PORT=13100
+export	CAMERA_IDX=0
+export	DELAY_US=100000
---
+++ cam/env_sim.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/sh
+
+export	ROB_ADDR=localhost
+export	ROB_PORT=13100
+export	CAMERA_IDX=0
+export	DELAY_US=100000
